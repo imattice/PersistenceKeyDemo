4ae73b61c34bc4161ca92c3831c11010e337558e
Ike Mattice <ikemattice@icloud.com>
2023-09-13 08:28:09 -0700

    Initial Commit

---
+++ PersistenceKeyDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersistenceKeyDemo
+//
+//  Created by Ike Mattice on 9/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersistenceKeyDemo/PersistenceKeyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistenceKeyDemoApp.swift
+//  PersistenceKeyDemo
+//
+//  Created by Ike Mattice on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PersistenceKeyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
