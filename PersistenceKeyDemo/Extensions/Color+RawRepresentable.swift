01d68be122695f9b0d6810e0873f6fc02ce28444
Ike Mattice <ikemattice@icloud.com>
2023-10-02 14:38:32 -0700

    Add Color+RawRepresentable

---
+++ PersistenceKeyDemo/Extensions/Color+RawRepresentable.swift
@@ -0,0 +1,37 @@
+//
+//  Color+RawRepresentable.swift
+//  PersistenceKeyDemo
+//
+//  Created by Ike Mattice on 10/2/23.
+//
+
+import Foundation
+
+import Foundation
+import SwiftUI
+import UIKit
+
+extension Color: RawRepresentable {
+    public init?(rawValue: String) {
+        guard let data = Data(base64Encoded: rawValue) else{
+            self = .black
+            return
+        }
+
+        do {
+            let color: UIColor = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: data) ?? .black
+            self = Color(color)
+        } catch {
+            self = .black
+        }
+    }
+
+    public var rawValue: String {
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: UIColor(self), requiringSecureCoding: false) as Data
+            return data.base64EncodedString()
+        } catch {
+            return ""
+        }
+    }
+}
